e1c5defc78987775eba120adc8d5d94212744548
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2021-12-14 15:57:26 -0300

    Cria o POD do core usando helm

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ Aplicação desenvolvida para demonstração de conceitos de DevOps
 ### Organização:
     o Projeto está organizado da seguinte maneira:
         - /main
-          - Contém o necessário para a aplicação CORE
+          - Contém o necessário (helm) para a aplicação CORE
         - /redis
-          - Contém o necessário para a database (REDIS)
+          - Contém o necessário (helm) para a database (REDIS)
         
+        - /src/main
+          - Contém o necessário (GoLang source code) para a aplicação CORE
+
         - /tests/ -> Contém os "testes" das aplicações
--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,6 @@
-# Aplicação mega simples somente para demostrar os conceitos de devOps
-# Constste em uma api qual irá consumir uma instancia REDIS para gerar respostas
-# Usando Golang devido a sua performance exemplar e simplicidade na construção de um WebServer simples
+// # Aplicação mega simples somente para demostrar os conceitos de devOps
+// # Constste em uma api qual irá consumir uma instancia REDIS para gerar respostas
+// # Usando Golang devido a sua performance exemplar e simplicidade na construção de um WebServer simples
 
 package main
 
@@ -13,16 +13,30 @@ func main() {
 	handler := http.NewServeMux()
 	fmt.Println("INICIANDO SERVIDOR")
 
-	handler.HandleFunc("/api/hello", Hello)
+	// Rotas basicas de chack
 	handler.HandleFunc("/api/status", Helth_check)
-	fmt.Println("INICIADO")
-	http.ListenAndServe("0.0.0.0:8080", handler)
-}
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `Hello world`)
+	// Rotas da aplicação CRUD
+	handler.HandleFunc("/api/get", get_db)
+	handler.HandleFunc("/api/put", insert_db)
+	handler.HandleFunc("/api/delete", remove_db)
+
+	http.ListenAndServe("0.0.0.0:8080", handler)
+	// Listen to all request to port 8080.
 }
 
 func Helth_check(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `Check OK!`)
+}
+
+func insert_db(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `STATUS: INSERIDO!`)
+}
+
+func remove_db(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `STATUS: REMOVIDO!`)
+}
+
+func get_db(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Check dado`)
 }
